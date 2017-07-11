b387b2b6db059a5f90694ec6b76ef339970ba6a3
Luis Valdez <you@example.com>
2017-07-11 08:42:52 -0600

    primer commit

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+	?>
+
+	<?php
+
+	$host_db = "localhost";
+	$user_db = "root";
+	$pass_db = "sr1920la";
+	$db_name = "Vehiculos";
+	$tbl_name = "Usuarios";
+		$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+	if ($conexion->connect_error) {
+	 die("La conexion falló: " . $conexion->connect_error);
+	}
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+  echo "$username";
+
+  if ($username == '')
+  {
+  //  header('Location: Error.html');
+  echo "error";
+  }
+
+
+
+	$sql = "SELECT * FROM $tbl_name WHERE Usuario = '$username'";
+
+	$result = $conexion->query($sql);
+
+
+	if ($result->num_rows > 0) {
+	 }
+	 $row = $result->fetch_array(MYSQLI_ASSOC);
+	 if ( $password == $row['Password'] ) {
+
+	    $_SESSION['loggedin'] = true;
+	    $_SESSION['username'] = $username;
+	    $_SESSION['start'] = time();
+	    $_SESSION['expire'] = $_SESSION['start'] + (15 * 60);
+
+	    echo "Bienvenido! " . $_SESSION['username'];
+	     header('Location: app.php');
+	 } else {
+	    // header('Location: Error.html');
+      echo "error2";
+
+
+	 }
+	 mysqli_close($conexion);
+	 ?>
